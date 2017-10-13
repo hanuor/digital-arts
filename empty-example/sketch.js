690fa8b5469dcf05eca9150ab08351730b648e78
hanuor <hanuortech@gmail.com>
2017-10-13 13:23:50 +0530

    Added code to occupy the whole screen

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,17 +1,17 @@
-function draw(){
-    createCanvas(1920,1080);
+function setup(){
+    createCanvas(windowWidth,windowHeight);
     background(255, 204, 0);
-
-    beginShape();
-    fill(255,191,0);
-    vertex(100,240);
-    vertex(200,180);
-    vertex(220,120);
-    vertex(160,140);
-    vertex(420,120);
-    vertex(320,160);
-    vertex(400, 180);
-    vertex(280, 200);
-    vertex(260,240);
-    endShape();
+//
+//    beginShape();
+//    fill(255,191,0);
+//    vertex(100,240);
+//    vertex(200,180);
+//    vertex(220,120);
+//    vertex(160,140);
+//    vertex(420,120);
+//    vertex(320,160);
+//    vertex(400, 180);
+//    vertex(280, 200);
+//    vertex(260,240);
+//    endShape();
 }
