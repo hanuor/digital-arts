a063bb26f6e18f3a3fd14ed1aa2e7c230e2c3067
hanuor <hanuortech@gmail.com>
2017-10-13 10:59:34 +0530

    Adding some color to life

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,6 +1,6 @@
 function draw(){
- createCanvas(150,150);
-    background(51);
+ createCanvas(250,250);
+    background(255, 204, 0);
 ellipse(50,50,100,100);
     rect(10,10,90,90);
     rect(20,20,70,70);
