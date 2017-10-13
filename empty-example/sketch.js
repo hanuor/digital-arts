edcc3f9c994d0ec7acce95f9e2229a891e00ea51
hanuor <hanuortech@gmail.com>
2017-10-13 11:37:08 +0530

    Added some lines of code

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,7 +1,7 @@
 function draw(){
- createCanvas(250,250);
+    createCanvas(300,300);
     background(255, 204, 0);
-ellipse(50,50,100,100);
-    rect(10,10,90,90);
+    ellipse(50,50,100,100);
+    rect(10,10,90,90)
     rect(20,20,70,70);
 }
